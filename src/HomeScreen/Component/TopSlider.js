be6fae7f8839f4f60bb593c5e7d0e8f3786e9343
Swapnil Mall <swapnilmall03@gmail.com>
2021-02-05 13:35:49 +0530

    made top 5 anime row

--- src/HomeScreen/Component/TopSlider.js
+++ src/HomeScreen/Component/TopSlider.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../Static/Slider.css'
 
 const TopSlider = () => {
     return (
-        <div>
-
+        <div className="row">
+            <div className="row_card one" />
+            <div className="row_card two" />
+            <div className="row_card three" />
+            <div className="row_card four" />
+            <div className="row_card five" />
         </div>
     );
 };
